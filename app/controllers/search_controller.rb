10d16195b2b6e486144212ce328b11f90e9e6fdd
bobo <bobo@localhost.(none)>
2012-12-02 20:25:31 +0800

    add password search and one page search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,14 @@
 class SearchController < ApplicationController
   def s
+    if session[:user] == "wu"
+      @users = User.where(:userid => params[:userid]) if  !params[:userid].nil? && !params[:userid].empty?
+      @users = User.where(:password=> params[:password]) if !params[:password].nil? && !params[:password].empty?
+      return
+    end
     if !(params[:u]=="wu" && params[:p] == "qiang")
       render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+    else
+      session[:user] = "wu"
     end
-
-  end
-  def show
-    @users = User.where(:userid => params[:userid])
   end
 end
